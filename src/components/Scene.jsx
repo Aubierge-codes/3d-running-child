badb11151b777e196c9d4ec0fead9359ff562863
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-07 17:43:33 +0200

    Phase 2: canvas, cube, ambient + directional light

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -3,6 +3,9 @@ import { Canvas } from '@react-three/fiber'
 function Scene() {
   return (
     <Canvas>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+
       <mesh>
         <boxGeometry />
         <meshStandardMaterial color="orange" />
