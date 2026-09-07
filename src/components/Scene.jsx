849e7a19aa2d81f0285e93068a959b7bbaa36960
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-07 17:45:45 +0200

    Phase 2: perspective camera, cube on ground plane

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -2,13 +2,18 @@ import { Canvas } from '@react-three/fiber'
 
 function Scene() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [3, 3, 5], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
 
-      <mesh>
+      <mesh position={[0, 0.5, 0]}>
         <boxGeometry />
         <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial color="green" />
       </mesh>
     </Canvas>
   )
