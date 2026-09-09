d8b9c6b68c328d8936b6d8a04fcf46bd3f365ed5
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-09 11:59:03 +0200

    Make scene true full screen: reset default margins, remove manual wrapper div

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import Scene from './components/Scene'
 
-
 function App() {
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Scene />
-    </div>
-  )
+  return <Scene />
 }
 
 export default App
